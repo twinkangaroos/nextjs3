74fbf8aa4de6674ce50294c40253db8579500b6f
twinkangaroos <132538451+twinkangaroos@users.noreply.github.com>
2024-02-02 13:49:35 +0900

    add get method for test.

--- app/api/save/route.ts
+++ app/api/save/route.ts
@@ -15,8 +15,75 @@ export const dynamic = 'force-dynamic' // defaults to auto
 import { type NextRequest, NextResponse } from 'next/server'
 // import { headers } from 'next/headers'
 
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
     console.log("Get Start..")
+    try {
+        // MySQL接続情報をSecretManagerから取得
+        const secret_name = "devMySQLUserOsaka";
+        const client = new SecretsManagerClient({
+            region: "ap-northeast-3",
+        })
+        const secretRes = await client.send(
+            new GetSecretValueCommand({
+                SecretId: secret_name,
+                VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+            })
+        )
+        const secret = secretRes.SecretString
+        const secretJson = secret ? JSON.parse(secret) : {}
+        const connection = mysql.createConnection({
+            host: secretJson.host,
+            user: secretJson.username,
+            password: secretJson.password,
+            database: 'post1'
+        });
+        const currentDate = new Date().toISOString()
+        console.log("host", secretJson.host)
+        console.log("Connect start... currentDate", currentDate)
+
+        connection.connect((err) => {
+            if (err) {
+                console.log('接続エラー:(' + currentDate + ') ' + err.stack);
+                const response = {
+                    statusCode: 501,
+                    body: {
+                        "result": "1",
+                        "errorCode": "200"
+                    }
+                }
+                return Response.json(response)
+            } else {
+                // JSON形式でDBに格納
+                const query = 'INSERT INTO ekyc2 (ekyc) values (?)'
+                const result = connection.query(query, insertJson);
+                console.log('データが挿入されました！');
+            }
+        })
+
+        const response = {
+            statusCode: 200,
+            body: {
+                "result": "0", 
+                "errorCode": ""
+            }
+        }
+        return Response.json(response)
+    }
+    catch(err){
+        console.log("An error has occured!!", err)
+        const response = {
+            statusCode: 500,
+            body: {
+                "result": "1",
+                "errorCode": "200"
+            }
+        }
+        return Response.json(response)
+    }
+}
+
+export async function POST(request: NextRequest) {
+    console.log("Post Start..")
     // const token = request.cookies.get('token')
     // const headersList = headers()
     // const referer = headersList.get('referer')
